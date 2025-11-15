675718d35cff67e6bb7300ae68dd00ba90d4cbed
Cuizihan1212 <1251001002@cnu.edu.cn>
2025-11-15 11:12:39 +0800

    Update 第一章，映射与函数.c

--- 第一章，映射与函数.c
+++ 第一章，映射与函数.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-// 定义一
+// 为了能正确传递表达式，需要一个解析器
+void parse_expression(char* expression){
+  for(int i=0;expression[i] != '\0';i++){
+       // wc,有点难，我先想想
+  }
+}
+
+
+
+// 定义一：映射
+//先空着，有思路回来
+
+
+
+
+
